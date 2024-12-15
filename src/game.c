4e1b08514fd84db8f5bc1e379e686f21ac5b1ecd
SimplyCEO <simplyceo.developer@gmail.com>
2024-12-14 22:07:42 -0300

    Added status text for progress bar visualisation

--- src/game.c
+++ src/game.c
@@ -8,6 +8,9 @@
 #include "game.h"
 #include "Application.h"
 
+char* progress_label_text = "Preparing to download...";
+GtkWidget* progress_label;
+
 void
 play_game(const char* mc_version, const char* mc_username, const char* mc_gamedir, const char* mc_class)
 {
@@ -30,6 +33,7 @@ void
 update_progress_bar(gpointer progress_bar)
 {
   gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_bar), (gdouble) dl_current / dl_total);
+  gtk_label_set_text(GTK_LABEL(progress_label), progress_label_text);
   /* I do have to say, I don't know why this is needed... Does not have any fucking sense! WHAT??? */
   if (dl_current == dl_total)
   { gtk_widget_destroy(GTK_WIDGET(progress_bar)); }
@@ -42,22 +46,22 @@ download_library(Library library)
 
   switch (library)
   {
-    case LWJGL:                   version = "2.9.3";            break;
-    case AUTHLIB:                 version = "1.5.25";           break;
-    case APACHE_COMMONS_IO:       version = "2.5";              break;
-    case APACHE_COMMONS_LANG3:    version = "3.12.0";           break;
-    case GSON:                    version = "2.7";              break;
-    case GUAVA:                   version = "19.0";             break;
-    case ICU4J:                   version = "65.1";             break;
-    case JOPT_SIMPLE:             version = "4.6";              break;
-    case LOG4J_CORE:              version = "2.0-beta9-fixed";  break;
-    case LOG4J_API:               version = "2.0-beta9-fixed";  break;
-    case PAULSCODE_CODECJORBIS:   version = "20101023";         break;
-    case PAULSCODE_CODECWAV:      version = "20101023";         break;
-    case PAULSCODE_JAVA_SOUND:    version = "20101123";         break;
-    case PAULSCODE_LWJGL_OPENAL:  version = "20100824";         break;
-    case PAULSCODE_SOUND_SYSTEM:  version = "20120107";         break;
-    case NETTY_AIO:               version = "4.0.56.Final";     break;
+    case LWJGL:                   { progress_label_text = "Downloading LWJGL...";                  version = "2.9.3"; }            break;
+    case AUTHLIB:                 { progress_label_text = "Downloading AuthLib...";                version = "1.5.25"; }           break;
+    case APACHE_COMMONS_IO:       { progress_label_text = "Downloading Commons-IO...";             version = "2.5"; }              break;
+    case APACHE_COMMONS_LANG3:    { progress_label_text = "Downloading Commons-Lang3...";          version = "3.12.0"; }           break;
+    case GSON:                    { progress_label_text = "Downloading Gson...";                   version = "2.7"; }              break;
+    case GUAVA:                   { progress_label_text = "Downloading Guava...";                  version = "19.0"; }             break;
+    case ICU4J:                   { progress_label_text = "Downloading ICU4J...";                  version = "65.1"; }             break;
+    case JOPT_SIMPLE:             { progress_label_text = "Downloading JOpt Simple...";            version = "4.6"; }              break;
+    case LOG4J_CORE:              { progress_label_text = "Downloading Log4J Core...";             version = "2.0-beta9-fixed"; }  break;
+    case LOG4J_API:               { progress_label_text = "Downloading Log4J API...";              version = "2.0-beta9-fixed"; }  break;
+    case PAULSCODE_CODECJORBIS:   { progress_label_text = "Downloading PaulsCode CodecJorbis...";  version = "20101023"; }         break;
+    case PAULSCODE_CODECWAV:      { progress_label_text = "Downloading PaulsCode CodecWAV...";     version = "20101023"; }         break;
+    case PAULSCODE_JAVA_SOUND:    { progress_label_text = "Downloading PaulsCode Java Sound...";   version = "20101123"; }         break;
+    case PAULSCODE_LWJGL_OPENAL:  { progress_label_text = "Downloading PaulsCode LWJGL OpenAL..."; version = "20100824"; }         break;
+    case PAULSCODE_SOUND_SYSTEM:  { progress_label_text = "Downloading PaulsCode Sound System..."; version = "20120107"; }         break;
+    case NETTY_AIO:               { progress_label_text = "Downloading Netty - All In One...";     version = "4.0.56.Final"; }     break;
     default: fprintf(stderr, "Unknown library.\n"); return 1;
   }
 
@@ -112,8 +116,11 @@ initialise_game(gpointer data)
   /* Start progress bar */
   dl_total = (Library)LIBRARY_LAST-1;
   GtkWidget* progress_window  = CApplication.Window.Create("Downloading dependencies...", 350, 70, GTK_WIN_POS_CENTER);
+  GtkWidget* progress_box     = CApplication.Box.Create(progress_window, GTK_ORIENTATION_VERTICAL);
+             progress_label   = gtk_label_new(progress_label_text);
   GtkWidget* progress_bar     = gtk_progress_bar_new();
-  gtk_container_add(GTK_CONTAINER(progress_window), progress_bar);
+  gtk_box_pack_start(GTK_BOX(progress_box), progress_label, FALSE, TRUE, 0);
+  gtk_box_pack_end(GTK_BOX(progress_box), progress_bar, FALSE, TRUE, 0);
   gtk_widget_show_all(progress_window);
   g_idle_add((GSourceFunc)update_progress_bar, progress_bar);
 
@@ -125,6 +132,7 @@ initialise_game(gpointer data)
   for (i=0; i<=(int)dl_total; i++)
   { download_library((Library)i); }
 
+  gtk_label_set_text(GTK_LABEL(progress_label), "Starting game...");
   gtk_widget_destroy(GTK_WIDGET(progress_window));
 
   play_game(mc_version, mc_username, mc_gamedir, mc_class);
